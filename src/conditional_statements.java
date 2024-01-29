3e43a78900843aeb4da3f33543af04a82f3794d2
sahilxvaish <sahilvxish@gmail.com>
2024-01-29 06:06:46 +0530

    commit java

--- src/conditional_statements.java
+++ src/conditional_statements.java
@@ -16,17 +16,34 @@ public class conditional_statements {
 //            System.out.println("not adult");
 //        }
 
-      Scanner sc = new Scanner (System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+//      Scanner sc = new Scanner (System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        if (a==b){
+//            System.out.println("equal");
+//        }else if (a>b) {
+//                System.out.println("a is  greater");
+//            } else{
+//                System.out.println("a is lesser");
+//            }
+//
 
-        if (a==b){
-            System.out.println("equal");
-        }else if (a>b) {
-                System.out.println("a is  greater");
-            } else{
-                System.out.println("a is lesser");
-            }
+        //switch
+
+        Scanner sc =new Scanner (System.in);
+        int button = sc.nextInt();
+
+        switch(button){
+            case 1 : System.out.println("Namaste");
+            break;
+            case 2 : System.out.println("Hello");
+            break;
+            case 3 : System.out.println("Bonjour");
+            break;
+            default : System.out.println("Invalid button");
+
+        }
 
 
 
