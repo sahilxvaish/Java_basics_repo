bedfb661362c4fc7a90604a03c25a7bf6585ca1e
sahilxvaish <sahilvxish@gmail.com>
2024-01-29 06:30:24 +0530

    commit java

---
+++ src/loops_01.java
@@ -0,0 +1,25 @@
+public class loops_01 {
+    public static void main(String[] args) {
+        // for loops
+        //while loops
+        //do while loops
+
+        //for loop.
+//        for(int i =0; i < 11; i = i++){
+//            System.out.println(i);
+//        }
+
+        //while loop
+
+//        int i= 0;
+//        while(i<11){
+//            System.out.println(i);
+//            i = i+1;
+//        }
+
+        //do while loop
+
+        int i = 0;
+
+    }
+}
