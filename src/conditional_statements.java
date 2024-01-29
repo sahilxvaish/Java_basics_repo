a10dea2d48e02ca63141a77bb1ba173420ec1c69
sahilxvaish <sahilvxish@gmail.com>
2024-01-29 05:39:05 +0530

    commit java

---
+++ src/conditional_statements.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class conditional_statements {
+    public static void main(String[] args) {
+        // if else
+        // else if
+        // switch
+        // break
+
+//        Scanner sc = new Scanner(System.in);
+//        int age = sc.nextInt();
+//
+//        if(age> 18){
+//            System.out.println("Adult");
+//
+//        }else{
+//            System.out.println("not adult");
+//        }
+
+      Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a==b){
+            System.out.println("equal");
+        }else if (a>b) {
+                System.out.println("a is  greater");
+            } else{
+                System.out.println("a is lesser");
+            }
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
