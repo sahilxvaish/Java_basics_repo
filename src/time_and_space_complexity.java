cf8805227fc72cdfd723dc679f0e7020fb323bbf
sahilxvaish <sahilvxish@gmail.com>
2024-02-09 01:28:08 +0530

    commit java

---
+++ src/time_and_space_complexity.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class time_and_space_complexity {
+    public static void main(String[] args) {
+        //time complexity
+        //it is a relation between input size and running time (operations).
+
+        //1 operation is equal to 1 unit of time
+
+        //  worst case O(n)
+        // masters theorem
+
+        Scanner sc = new Scanner(System.in);
+
+        // n
+        // n^2
+        // n^3
+
+        //dynamic programming
+
+
+    }
+}
