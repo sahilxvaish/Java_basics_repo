fbbb88a48b1fdda18539b83b395197474bdbc22a
sahilxvaish <sahilvxish@gmail.com>
2024-02-12 18:33:48 +0530

    commit java

---
+++ src/operator_and_binary_number_sys.java
@@ -0,0 +1,6 @@
+public class operator_and_binary_number_sys {
+    public static void main(String[] args) {
+        //operators
+        //symbols that tell compiler to perform some operation.
+    }
+}
