2cfdb707eea58b868f0ee80ad67e2b0fc87095b5
sahilxvaish <sahilvxish@gmail.com>
2024-02-13 15:45:24 +0530

    commit java

--- src/operator_and_binary_number_sys.java
+++ src/operator_and_binary_number_sys.java
@@ -2,5 +2,22 @@ public class operator_and_binary_number_sys {
     public static void main(String[] args) {
         //operators
         //symbols that tell compiler to perform some operation.
+
+        //Arithmetic operators
+        //binary [+, -, *, /, % (modular) modular gives remainder]
+        //unary[++, --]
+
+        //Relational operators
+        //[==, !=(not equal), >, <, >=, <=]
+
+        //logical operators
+        //[&&(AND), ||(OR), !(NOT)]
+
+        //BINARY NUMBER SYSTEM
+        //bitwise operators
+        //[&, |, ^(binary XOR), ~(binary one"s compliment), <<(left shift), >>(right shift)]
+
+        //Assignment operators
+        //[=, +=, -=, *=, /=]
     }
 }
